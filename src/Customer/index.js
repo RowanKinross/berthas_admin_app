74a70777626dd93a3f459899e67efe5b783978e4
RowanKinross <rowankinross@yahoo.co.uk>
2024-04-24 16:30:50 +0100

    main page added

---
+++ src/Customer/index.js
@@ -0,0 +1,13 @@
+function Customer() {
+  return (
+    <div>
+      <h2>Customer Page</h2>
+      <ul>
+        {/* <li><Link to="/order-history">Order History</Link></li>
+        <li><Link to="/new-order">New Order</Link></li> */}
+      </ul>
+    </div>
+  );
+}
+
+export default Customer;
