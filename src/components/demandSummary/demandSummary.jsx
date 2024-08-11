895238cf5e15c2bf2ffa26c6c81adb1b40e8b00b
RowanKinross <rowankinross@yahoo.co.uk>
2024-08-11 13:58:54 +0100

    demand summary change

--- src/components/demandSummary/demandSummary.jsx
+++ src/components/demandSummary/demandSummary.jsx
@@ -1,17 +1,17 @@
-import { useState, useEffect} from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import './demandSummary.css'
 
-const demandSummary = []
 
 function DemandSummary() {
   return (
     <div className='demandSummary'>
       <h2>DEMAND SUMMARY</h2>
-      {demandSummary.length > 0 ? (
+      {/* {demandSummary.length > 0 ? (
         <p>{demandSummary}</p>
       ):(
         <p className='py-3'>Loading demand summary...</p>
-      )}
+      )} */}
+      <p className='py-3'>Loading demand summary...</p>
     </div>
   )
 }
