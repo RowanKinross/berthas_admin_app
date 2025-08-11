cf553ec3052d222e8fca0b2c8457865dfb7a25ce
RowanKinross <rowankinross@yahoo.co.uk>
2025-08-11 17:31:23 +0100

    moved sort function to after the mapping on pizzas

--- src/components/orders/Orders.jsx
+++ src/components/orders/Orders.jsx
@@ -805,7 +805,13 @@ const handlePrintClick = () => {
                 <span className="slider round"></span>
               </label>
             </div>
-            {Object.entries(selectedOrder.pizzas).map(([pizzaName, pizzaData], index) => (
+            {Object.entries(selectedOrder.pizzas)
+              .sort(([a], [b]) => {
+                const nameA = pizzaTitles[a] || a;
+                const nameB = pizzaTitles[b] || b;
+                return nameA.localeCompare(nameB);
+              })
+              .map(([pizzaName, pizzaData], index) => (
             <div key={index} className='pizzasOrdered'>
               {/* Show pizza name and total quantity ONCE */}
               <div className='flexRow'>
