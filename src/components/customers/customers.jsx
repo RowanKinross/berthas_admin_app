1ac8a25ebbdb9b28d31d00bf5c2f2890eedc064b
RowanKinross <rowankinross@yahoo.co.uk>
2025-11-07 18:48:40 +0000

    customers dropdown scrollable in mobile

--- src/components/customers/customers.jsx
+++ src/components/customers/customers.jsx
@@ -315,48 +315,21 @@ const Customers = ({ accountID }) => {
             ▼
           </button>
           {showDropdown && (
-            <div className="customer-dropdown" style={{
-              position: 'absolute',
-              top: '100%',
-              left: 0,
-              right: 0,
-              backgroundColor: 'white',
-              border: '1px solid #ccc',
-              borderTop: 'none',
-              maxHeight: '200px',
-              overflowY: 'auto',
-              zIndex: 1000
-            }}>
-              
+            <div className="customer-dropdown">
               {/* Existing customers */}
               {filteredCustomers.map(account => (
                 <div
                   key={account.id}
+                  className="customer-dropdown-item"
                   onClick={() => handleCustomerSelect(account)}
-                  style={{
-                    padding: '8px 12px',
-                    cursor: 'pointer',
-                    borderBottom: '1px solid #eee'
-                  }}
-                  onMouseEnter={(e) => e.target.style.backgroundColor = '#f5f5f5'}
-                  onMouseLeave={(e) => e.target.style.backgroundColor = 'white'}
                 >
                   {account.customer}
                 </div>
               ))}
               {/* Add New Customer option */}
               <div
+                className="customer-dropdown-item add-new"
                 onClick={() => handleCustomerSelect('ADD_NEW')}
-                style={{
-                  padding: '8px 12px',
-                  cursor: 'pointer',
-                  borderBottom: '1px solid #eee',
-                  backgroundColor: '#f0f8ff',
-                  fontWeight: 'bold',
-                  color: '#007bff'
-                }}
-                onMouseEnter={(e) => e.target.style.backgroundColor = '#e6f3ff'}
-                onMouseLeave={(e) => e.target.style.backgroundColor = '#f0f8ff'}
               >
                 + Add New Customer
               </div>
